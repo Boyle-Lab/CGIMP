977b7da8d38cf8827d18077a6859bfacc9619ce2
adadiehl <adadiehl@umich.edu>
2019-06-04 10:07:05 -0400

    Added more extensive comments within modules_to_nodes.py

--- data/modules_to_nodes.py
+++ data/modules_to_nodes.py
@@ -5,6 +5,16 @@ import re
 import json
 
 """
+This script produces the nodes.json file in the CGIMP example dataset.
+nodes.json contains descriptive information for the nodes in the map
+image. There is no fixed format for this file and it can include labels, 
+lists of attributes, etc. The only required fields are "id," which is
+a unique numerical identifier corresponding the node's location on the
+map grid, and "modules," the list of module ID numbers assigned to the
+node.
+"""
+
+"""
 This code is part of the CGIMP distribution
 (https://github.com/Boyle-Lab/CGIMP) and is governed by its license.
 Please see the LICENSE file that should have been included as part of this
@@ -23,14 +33,15 @@ GNU General Public License for more details.
 CONTACT: Adam Diehl, adadiehl@umich.edu
 """
 
-def node_init(key, vals):
+def node_init(key, id, vals):
     """
     Initialize a new node document.
     """
-    
-    node = {
-        "id": key,
-        "modules": [vals[3]],
+    node = {        
+        "id": key,  # The node ID
+        "modules": [id],  # New list with single entry for current ID
+        # The rest of these are optional and you can replace them with
+        # whatever you want from your dataset.
         "class": vals[7].strip('"'),
         "factors": vals[6].split("-")
     }
@@ -41,22 +52,29 @@ def parse_rec(rec, res):
     Parse a single record and return a dictionary representing a single database document.
     """
     vals = rec.split("\t")
-    id = vals[3]
-    node = vals[5]
+    # Edit the list indices in the following two lines to match the locations
+    # of the respective fields in your data
+    id = vals[3]    # The unique module ID for the current record
+    node = vals[5]  # The ID of the node to which this module belongs
 
+    # If the current nodes is already in our dictionary, append the current
+    # module ID to its modules list. Otherwise, initialize a new node
+    # document in the dictionary.
     if node in res.keys():
         res[node]["modules"].append(id)
     else:
-        res[node] = node_init(node, vals)
+        res[node] = node_init(node, id, vals)
 
 if __name__ == "__main__":
-    # Parse lines in the file into a list of dictionaries that will be converted to JSON output
+    # Parse lines in the file into a dictionary of node documents that will
+    # be written to STDOUT as a JSON string
     res = {}
     with open(sys.argv[1]) as f:
         for rec in f:
             # Skip comment lines
             if not re.search('^#', rec):
                 parse_rec(rec.strip(), res)
-
+                
+    # Write the JSON string to STDOUT
     sys.stdout.write("{}\n".format(json.dumps(res)))
     
